a5c5e95463c9bc5255bd44fde82329ec966595fb
zwelisibanda <zwelistats@gmail.com>
2025-05-12 11:55:24 +0200

    Functions completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
+        
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome(){
+              Console.WriteLine("Welcome to the program!");
+        }
+        static String PromptUserName(){
+              Console.WriteLine("Please enter your name : ");
+              string name = Console.ReadLine();
+              return name;
+        }
+        static int PromptUserNumber(){
+              Console.WriteLine("Please enter favorite number : ");
+              int favoriteNumber = int.Parse(Console.ReadLine());
+              return favoriteNumber;
+        }
+        static int SquareNumber(int numb){
+            int squareRoot = (int)Math.Sqrt(numb);
+            return squareRoot;
+
+        }
+        static void DisplayResult(String name, int squaredNumber){
+            Console.WriteLine($"Hello {name}+ Your favorite number`s square root is  {squaredNumber}");
+
+        }
+
     }
 }
