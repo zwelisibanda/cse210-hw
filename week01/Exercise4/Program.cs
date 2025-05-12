71b933254ee4cbcddd467e9e36cc9da96071104c
zwelisibanda <zwelistats@gmail.com>
2025-05-12 09:54:18 +0200

    first try

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<int> numbers = new List<Int>();
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        int numInput = Int32.Parse(Console.ReadLine());
+        while(numInput != 0){
+            numbers.Append(numInput);
+        }
     }
 }
