ed9a7de73d64bba5a9e34af045e5da1e4ee7c242
zwelisibanda <zwelistats@gmail.com>
2025-05-12 09:03:59 +0200

    Managed to understand the flow

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -8,17 +8,17 @@ class Program
     static void Main(string[] args)
     {
     string response = "";
+    int guess  = -1;
         Console.WriteLine("What is the magic number");
         int magic_number = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("What is your guess?");
-        int guess = Int32.Parse(Console.ReadLine());
+        
         while (guess != magic_number){
-            Console.Write("What is you guess?");
+            Console.Write("What is you guess? ");
             guess = Int32.Parse(Console.ReadLine());
             if(magic_number > guess){
-                Console.Write("Higher");
+                Console.Write("Higher ");
             }else if(magic_number < guess){
-                Console.Write("Lower");
+                Console.Write("Lower !");
 
             }else{
                 Console.WriteLine("You guessed it!");
