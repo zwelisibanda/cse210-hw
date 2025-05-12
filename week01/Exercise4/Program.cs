eeef4cd98558d8a29af9ae6bd2f94ff25ffba275
zwelisibanda <zwelistats@gmail.com>
2025-05-12 10:43:33 +0200

    flow importance

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,12 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers = new List<Int>();
+        List<int> numbers = new List<int>();
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
-        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
-        int numInput = Int32.Parse(Console.ReadLine());
+        int numInput = -1;
         while(numInput != 0){
-            numbers.Append(numInput);
+            Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+            numInput = Int32.Parse(Console.ReadLine());
+             if(numInput != 0){
+                numbers.Add(numInput);
+             }
         }
+         int sum  = 0;
+         foreach (int num in numbers){
+            sum += num;
+
+         }
+         Console.WriteLine("The sum is :"+sum);
+         float av = ((float)sum / numbers.Count);
+         Console.WriteLine("The average is : "+av);
+         int start = 0;
+         int max = numbers[start];
+        
+        foreach(int num in numbers){
+            if (num > max){
+                max = num;
+            }
+        }
+        Console.WriteLine("The max is : "+max);
     }
+
 }
